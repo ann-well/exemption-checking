ad1c11ffa212607f52b4d1be82071eb6451426f6
Anna Welpa <anna.welpa@gmail.com>
2022-08-16 19:58:09 +0200

    Add no file error handling
    See: #8

--- dailyExemption.py
+++ dailyExemption.py
@@ -10,6 +10,7 @@ import time
 import openpyxl
 from openpyxl.styles import PatternFill
 import send2trash
+from tkinter import messagebox
 
 # Create window for simple GUI
 window = tk.Tk()
@@ -23,44 +24,48 @@ image_label = ttk.Label(
 image_label.pack()
 
 # Open daily report file
-wb = openpyxl.load_workbook('Expired Exemption with Email.xlsx')
-sheet = wb['Page1_1']
+try:
+    wb = openpyxl.load_workbook('Expired Exemption with Email.xlsx')
+    sheet = wb['Page1_1']
 
-# Determine the output file name from the date of the report
-reportDate = sheet.cell(row=sheet.max_row, column =1).value
-fileName = str(reportDate.day).zfill(2) + str(reportDate.month).zfill(2) + str(reportDate.year)
+    # Determine the output file name from the date of the report
+    reportDate = sheet.cell(row=sheet.max_row, column=1).value
+    fileName = str(reportDate.day).zfill(2) + str(reportDate.month).zfill(2) + str(reportDate.year)
+except:  # No file error handling
+    messagebox.showwarning("No file detected",
+                           "No file Expired Exemption with Email.xlsx found.\nPlease exit and put the file in this folder")
+
 
 # Function to run from tk inter window
 def checkExemption():
-    # Extract the data from file
     day = []
     for i in range(3, sheet.max_row):
-        day.append(sheet.cell(row= i, column= 1).value)   
+        day.append(sheet.cell(row=i, column=1).value)
     month = []
     for i in range(3, sheet.max_row):
-        month.append(sheet.cell(row= i, column= 2).value)
+        month.append(sheet.cell(row=i, column=2).value)
     year = []
     for i in range(3, sheet.max_row):
-        year.append(sheet.cell(row= i, column= 3).value)
+        year.append(sheet.cell(row=i, column=3).value)
     name = []
     for i in range(3, sheet.max_row):
-        name.append(sheet.cell(row= i, column= 4).value)
+        name.append(sheet.cell(row=i, column=4).value)
     lastname = []
     for i in range(3, sheet.max_row):
-        lastname.append(sheet.cell(row= i, column= 5).value)
+        lastname.append(sheet.cell(row=i, column=5).value)
     code = []
     for i in range(3, sheet.max_row):
-        code.append(sheet.cell(row= i, column= 6).value)
+        code.append(sheet.cell(row=i, column=6).value)
     siebel = []
     for i in range(3, sheet.max_row):
-        siebel.append(sheet.cell(row= i, column= 7).value)
+        siebel.append(sheet.cell(row=i, column=7).value)
 
     # Add column for output dates
-    sheet.insert_cols(8)
+    sheet.insert_cols(8)  # add a column for expiry dates
     sheet.column_dimensions['H'].width = 30
 
     # Create pattern fills
-    fillRed = PatternFill(patternType='solid', fgColor='FFC7CE')
+    fillRed = PatternFill(patternType='solid', fgColor='FFC7CE')  # create pattern fills
     fillGreen = PatternFill(patternType='solid', fgColor='C6EFCE')
 
     # Open NHS site
@@ -93,48 +98,47 @@ def checkExemption():
             time.sleep(0.5)
 
             codeWeb = browser.find_element('id', 'postcode')
-            try:  
+            try:
                 codeWeb.send_keys(code[i])
                 codeWeb.submit()
                 time.sleep(0.5)
             except:  # No postcode error handling
                 browser.get("https://services.nhsbsa.nhs.uk/check-my-nhs-exemption/start")
-                sheet.cell(row=i+3, column =8).value = 'No postcode'
+                sheet.cell(row=i + 3, column=8).value = 'No postcode'
                 for y in range(1, 17):
-                    sheet.cell(row=i+3, column=y).fill = fillRed
+                    sheet.cell(row=i + 3, column=y).fill = fillRed
                 continue
         except:  # Merged/duplicate cells error handling
-            if sheet.cell(row=i+2, column=y).fill == fillRed:
+            if sheet.cell(row=i + 2, column=y).fill == fillRed:
                 for y in range(1, 17):
-                    sheet.cell(row=i+3, column=y).fill = fillRed
+                    sheet.cell(row=i + 3, column=y).fill = fillRed
             browser.get("https://services.nhsbsa.nhs.uk/check-my-nhs-exemption/start")
             continue
 
         # Result of submitting the form
-        result = browser.find_element(By.CSS_SELECTOR, '.nhsuk-heading-xl').text
+        result = browser.find_element(By.CSS_SELECTOR, '.nhsuk-heading-xl').text  # webbrowser result
 
         # NHS exemption present
         if result == 'You currently have an NHS exemption':
-            exemptionDate = browser.find_element(By.CSS_SELECTOR,'.exemption-done-panel > h2:nth-child(2)').text
-            empty, expireText, date = exemptionDate.partition('Expires on ')
+            exemptionDate = browser.find_element(By.CSS_SELECTOR, '.exemption-done-panel > h2:nth-child(2)').text
+            empty, expireText, date = exemptionDate.partition('Expires on ')  # convert date to siebel-ready format
             actualDate = datetime.datetime.strptime(date, '%d %B %Y')
-            sheet.cell(row=i+3, column =8).value = actualDate.strftime('%d.%m.%Y')
+            sheet.cell(row=i + 3, column=8).value = actualDate.strftime('%d.%m.%Y')
             for y in range(1, 17):
-                sheet.cell(row=i+3, column=y).fill = fillGreen
+                sheet.cell(row=i + 3, column=y).fill = fillGreen
 
         # Over 60 years old
         elif result == 'You get help with health costs':
-            sheet.cell(row=i+3, column =8).value = '60 years old'
+            sheet.cell(row=i + 3, column=8).value = '60 years old'
             for y in range(1, 17):
-                sheet.cell(row=i+3, column=y).fill = fillGreen
+                sheet.cell(row=i + 3, column=y).fill = fillGreen
 
         # No information about the patient
         elif result == '''We couldn't match you to our records''':
             for y in range(1, 17):
-                sheet.cell(row=i+3, column=y).fill = fillRed
+                sheet.cell(row=i + 3, column=y).fill = fillRed
 
-        # Get back to main page
-        end = browser.find_element(By.CSS_SELECTOR, '.nhsuk-action-link__text')
+        end = browser.find_element(By.CSS_SELECTOR, '.nhsuk-action-link__text')  # back to front page
         end.click()
         time.sleep(1)
 
@@ -144,10 +148,15 @@ def checkExemption():
     # Delete input file
     send2trash.send2trash('Expired Exemption with Email.xlsx')
 
+    # Close windows
+    window.destroy()
+
+
 # Start the function by user input
 addButton = tk.Button(window,
-text = "START", 
-command = checkExemption)
+                      text="START",
+                      command=checkExemption)
 addButton.pack()
 
 window.mainloop()
+
